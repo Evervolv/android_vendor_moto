864cf1f4c203c1cc0106433d1a52fe4d55776c59
William Bellavance <flintman@flintmancomputers.com>
2017-12-13 21:13:43 -0500

    Revert "potter: switch to O 64bit qseecomd"
    This reverts commit 4aa11e70811f2b604124deddb0e40b97190c66b5.
    
    Change-Id: Iad99dd9f8f186621ad6f6dd3103d3f2d1619e084

--- potter/potter-vendor.mk
+++ potter/potter-vendor.mk
@@ -1124,15 +1124,6 @@ PRODUCT_COPY_FILES += \
     vendor/moto/potter/proprietary/vendor/lib64/libnetmgr.so:system/vendor/lib64/libnetmgr.so \
     vendor/moto/potter/proprietary/vendor/lib64/libqdi.so:system/vendor/lib64/libqdi.so \
     vendor/moto/potter/proprietary/vendor/lib64/libxml.so:system/vendor/lib64/libxml.so \
-    vendor/moto/potter/proprietary/vendor/lib64/libdrmfs.so:system/vendor/lib64/libdrmfs.so \
-    vendor/moto/potter/proprietary/vendor/lib64/libdrmtime.so:system/vendor/lib64/libdrmtime.so \
-    vendor/moto/potter/proprietary/vendor/lib64/libGPreqcancel.so:system/vendor/lib64/libGPreqcancel.so \
-    vendor/moto/potter/proprietary/vendor/lib64/libGPreqcancel_svc.so:system/vendor/lib64/libGPreqcancel_svc.so \
-    vendor/moto/potter/proprietary/vendor/lib64/libqisl.so:system/vendor/lib64/libqisl.so \
-    vendor/moto/potter/proprietary/vendor/lib64/libQSEEComAPI.so:system/vendor/lib64/libQSEEComAPI.so \
-    vendor/moto/potter/proprietary/vendor/lib64/librpmb.so:system/vendor/lib64/librpmb.so \
-    vendor/moto/potter/proprietary/vendor/lib64/libSecureUILib.so:system/vendor/lib64/libSecureUILib.so \
-    vendor/moto/potter/proprietary/vendor/lib64/libssd.so:system/vendor/lib64/libssd.so \
     vendor/moto/potter/proprietary/vendor/lib/libgralloc1.so:system/vendor/lib/libgralloc1.so
 
 PRODUCT_PACKAGES += \
