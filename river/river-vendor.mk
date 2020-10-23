e575ecd0bbc837128ad64e3ab0cb994d9fe9693c
Deivid21 <david.parra.ignacio@gmail.com>
2020-10-23 03:05:35 -0300

    sdm632-common: river : fix big circle in charging animation
    river: update charge_only_mode from QPUS30.52-16-2-9
    channel: update charge_only_mode from QPY30.52-22-8
    ocean: update charge_only_mode from QPO30.52-29-7
    
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>
    Change-Id: If801894b4c60c66112a9bb87b88673d696ef261d

--- river/river-vendor.mk
+++ river/river-vendor.mk
@@ -18,6 +18,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/moto/river
 
 PRODUCT_COPY_FILES += \
+    vendor/moto/river/proprietary/vendor/bin/charge_only_mode:$(TARGET_COPY_OUT_VENDOR)/bin/charge_only_mode \
     vendor/moto/river/proprietary/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service-ets:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.biometrics.fingerprint@2.1-service-ets \
     vendor/moto/river/proprietary/vendor/bin/hw/motorola.hardware.audio.adspd@1.0-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/motorola.hardware.audio.adspd@1.0-service \
     vendor/moto/river/proprietary/vendor/etc/acdbdata/Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Bluetooth_cal.acdb \
